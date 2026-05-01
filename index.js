def5545f6b3c6e56f52a9263977ce1feeaecee4c
Mohd Saad <159754906+Mohd-Saad30@users.noreply.github.com>
2026-05-01 19:43:45 +0530

    Update CORS configuration for thechoicestore.in

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser()); // Cookies read karne ke liye
 // CORS Configuration (Frontend connectivity ke liye best practice)
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173","https://super-store-lac.vercel.app/","thechoicestore.in"], 
+    origin: ["http://localhost:3000", "http://localhost:5173","https://super-store-lac.vercel.app/","https://www.thechoicestore.in/"], 
     credentials: true,
   })
 );
